9bc9b204a118d8dade88ad01ab1e8afce4d6b282
Andika Eka Kurnia <73420758+DikDns@users.noreply.github.com>
2022-01-20 12:04:38 +0700

    Variable 🟢

---
+++ 01-Variable/main.py
@@ -0,0 +1,20 @@
+# Variable
+a = 10
+b = 5
+panjang = 10000
+
+print("Nilai a = ", a)
+print("Nilai b = ", b)
+print("Nilai panjang = ", panjang)
+
+# aturan penulisan variable
+nilai_phi = 3.14    # tidak boleh ada spasi
+nilaiPhi = 3.14     # ini boleh
+juta10 = 10000000   # angka tidak boleh di awal nama
+
+print("Nilai nilai_phi = ", nilai_phi)
+
+# merubah nilai phi
+nilai_phi = 6
+
+print("Nilai nilai_phi = ", nilai_phi)
