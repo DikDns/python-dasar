f5c3161b9b688ad04ca273300f9e8904606e5750
Andika Eka Kurnia <73420758+DikDns@users.noreply.github.com>
2022-01-20 12:05:00 +0700

    Data Types in Python 🟢

---
+++ 02-TipeData/main.py
@@ -0,0 +1,19 @@
+data_integer = 1
+print("Data : ", data_integer)
+print("> Bertipe : ", type(data_integer))
+
+data_float = 3.14
+print("Data : ", data_float)
+print("> Bertipe : ", type(data_float))
+
+data_boolean = True
+print("Data : ", data_boolean)
+print("> Bertipe : ", type(data_boolean))
+
+data_string = "True 010"
+print("Data : ", data_string)
+print("> Bertipe : ", type(data_string))
+
+data_kompleks = complex(5, 5)
+print("Data : ", data_kompleks)
+print("> Bertipe : ", type(data_kompleks))
